1edac78f686f41ed8bdb1980e0254fbb1c719763
jpena9 <61855795+jpena9@users.noreply.github.com>
2021-05-11 13:02:02 -0400

    Add files via upload

---
+++ SecantMethodProject.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+int power;
+int coeff;
+
+cout<<"Input Degree Polynomial"<<endl;
+
+cin>>power;
+
+cout<<"Input coefficients"<<endl;
+
+
+
+return 0;
+}
